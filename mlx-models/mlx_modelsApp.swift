b94fa45ec7bf073f240eadd7881ead3d2ceb5a7d
Qifan Zhang <firefighter-eric@users.noreply.github.com>
2026-03-22 23:28:21 +0800

    Initial Commit

---
+++ mlx-models/mlx_modelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mlx_modelsApp.swift
+//  mlx-models
+//
+//  Created by Qifan Zhang on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct mlx_modelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
